9a45f2b08ad3374e4efc164cc1b6add0848ce567
Matheus De Lima Gomes <matheusdelimagomes@gmail.com>
2023-03-08 18:53:15 -0300

    feat: initial commit.

---
+++ src/main/java/com/example/calculatorapispringboot/CalculatorApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculatorapispringboot/CalculatorApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatorapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
